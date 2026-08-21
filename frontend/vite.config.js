13829b1401a01863741da8f547e89edebd5238d7
Saurabh Bhandari <bhandarisaurabh500@gmail.com>
2026-08-21 15:11:53 +0530

    fix: update base path for vercel deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/Varad-HospitalManagment/' : '/',
+export default defineConfig({
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
   ],
-}))
+})
